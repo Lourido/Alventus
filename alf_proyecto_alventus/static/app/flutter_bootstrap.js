9e642eb3b7b63dd2956ab4fed3e22e1bbcc12901
Lourido <23460076+Lourido@users.noreply.github.com>
2026-09-11 08:47:11 +0200

    Actualiza PWA compilada - 11/09/2026  8:47:11,29

--- alf_proyecto_alventus/static/app/flutter_bootstrap.js
+++ alf_proyecto_alventus/static/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4187926432" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2795965343" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
